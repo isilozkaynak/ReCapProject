64066cdef01d75fdbd31c6702d1c23b16fd577cb
Işıl Özkaynak <isil3519@gmail.com>
2021-08-17 21:31:03 +0300

    added controller

--- Business/Constants/Message.cs
+++ Business/Constants/Message.cs
@@ -19,16 +19,54 @@ namespace Business.Constants
 
         public static string MaintenanceTime = "Bakım zamanı";
 
+        public static string InvalidDateTime = "Geçersiz zaman bilgisi";
+
 
         //BRAND
+        public static string BrandAdded = "Marka sisteme eklendi.";
 
+        public static string BrandUpdated = "Marka bilgisi güncellendi.";
 
+        public static string BrandDeleted = "Marka sistemden silindi.";
 
-
+        public static string BrandListed = "Markalar listelendi.";
 
         //COLOR
 
+        public static string ColorAdded = "Renk sisteme eklendi.";
 
+        public static string ColorUpdated = "Renk bilgisi güncellendi.";
+
+        public static string ColorDeleted = "Renk sistemden silindi.";
+
+        public static string ColorListed = "Renkler listelendi.";
+
+        //USER
+        public static string UserAdded = "Kullanıcı sisteme eklendi.";
+
+        public static string UserUpdated = "Kullanıcı bilgisi güncellendi.";
+
+        public static string UserDeleted = "Kullanıcı sistemden silindi.";
+
+        public static string UserListed = "Kullanıcılar listelendi.";
+
+        //RENTAL
+        public static string RentalAdded = "Kiralama sisteme eklendi.";
+
+        public static string RentalUpdated = "Kiralama bilgisi güncellendi.";
+
+        public static string RentalDeleted = "Kiralama sistemden silindi.";
+
+        public static string RentalListed = "Tüm kiralamalar listelendi.";
+
+        //CUSTOMER
+        public static string CustomerAdded = "Müşteri sisteme eklendi.";
+
+        public static string CustomerUpdated = "Müşteri bilgisi güncellendi.";
+
+        public static string CustomerDeleted = "Müşteri sistemden silindi.";
+
+        public static string CustomerListed = "Müşteriler listelendi.";
 
     }
 }
